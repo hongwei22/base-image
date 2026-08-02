658145aecee46e45c7e71dad9097d5c1cbe8db61
hongwei22 <46392657+hongwei22@users.noreply.github.com>
2026-08-02 09:01:31 +0800

    Update default.sh

--- derivatives/pytorch/derivatives/comfyui/provisioning_scripts/default.sh
+++ derivatives/pytorch/derivatives/comfyui/provisioning_scripts/default.sh
@@ -52,8 +52,8 @@ DIFFUSION_MODELS=(
     #"https://huggingface.co/Comfy-Org/Qwen-Image-Edit_ComfyUI/resolve/main/split_files/diffusion_models/qwen_image_edit_2511_fp8mixed.safetensors?download=true"
     #"https://civitai.com/api/download/models/2612554?type=Model&format=SafeTensor&size=full&fp=bf16"
     "https://civitai.com/api/download/models/2606187?type=Model&format=SafeTensor&size=pruned&fp=fp8"
-    "https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_high_lighting_fp8_e4m3fn_v3.0.safetensors?download=true"
-    "https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_low_lighting_fp8_e4m3fn_v3.0.safetensors?download=true"
+    #"https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_high_lighting_fp8_e4m3fn_v3.0.safetensors?download=true"
+    #"https://huggingface.co/FX-FeiHou/wan2.2-Remix/resolve/main/NSFW/Wan2.2_Remix_NSFW_i2v_14b_low_lighting_fp8_e4m3fn_v3.0.safetensors?download=true"
 )
 
 SCHP=(
@@ -68,8 +68,8 @@ LORA_MODELS=(
     #"https://huggingface.co/ali-vilab/ACE_Plus/resolve/main/portrait/comfyui_portrait_lora64.safetensors?download=true"
     #"https://civitai.com/api/download/models/981081?type=Model&format=SafeTensor"
     #"https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Wan22_relight/WanAnimate_relight_lora_fp16.safetensors"
-    "https://huggingface.co/lightx2v/Wan2.2-Lightning/resolve/main/Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/high_noise_model.safetensors?download=true"
-    "https://huggingface.co/lightx2v/Wan2.2-Lightning/resolve/main/Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/low_noise_model.safetensors?download=true"
+    #"https://huggingface.co/lightx2v/Wan2.2-Lightning/resolve/main/Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/high_noise_model.safetensors?download=true"
+    #"https://huggingface.co/lightx2v/Wan2.2-Lightning/resolve/main/Wan2.2-I2V-A14B-4steps-lora-rank64-Seko-V1/low_noise_model.safetensors?download=true"
     #"https://huggingface.co/lightx2v/Qwen-Image-Edit-2511-Lightning/resolve/main/Qwen-Image-Edit-2511-Lightning-4steps-V1.0-bf16.safetensors?download=true"
     #"https://civitai.com/api/download/models/2653561?type=Model&format=SafeTensor"
     #"https://civitai.com/api/download/models/2646182?type=Model&format=SafeTensor"
@@ -77,8 +77,8 @@ LORA_MODELS=(
     #"https://civitai.com/api/download/models/2753532?type=Model&format=SafeTensor"
     #"https://civitai.com/api/download/models/2810006?type=Model&format=SafeTensor"
     "https://civitai.red/api/download/models/2960556?fileId=2839878"
-    "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_HIGH_lora_rank_128_fp16.safetensors?download=true"
-    "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_LOW_lora_rank_128_fp16.safetensors?download=true"
+    #"https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_HIGH_lora_rank_128_fp16.safetensors?download=true"
+    #"https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/LoRAs/Stable-Video-Infinity/v2.0/SVI_v2_PRO_Wan2.2-I2V-A14B_LOW_lora_rank_128_fp16.safetensors?download=true"
 )
 
 VAE_MODELS=(
@@ -93,7 +93,7 @@ CONTROLNET_MODELS=(
 )
 
 CLIP_MODELS=(
-    "https://huggingface.co/NSFW-API/NSFW-Wan-UMT5-XXL/resolve/main/nsfw_wan_umt5-xxl_fp8_scaled.safetensors?download=true"
+    #"https://huggingface.co/NSFW-API/NSFW-Wan-UMT5-XXL/resolve/main/nsfw_wan_umt5-xxl_fp8_scaled.safetensors?download=true"
 )
 
 TEXT_ENCODER_MODELS=(
